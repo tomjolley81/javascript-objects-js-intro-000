a8218ebda16aa471ab91c6a67d7ab07792baaed9
Thomas Jolley <tomjolley81@gmail.com>
2018-11-07 21:06:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,10 @@
 var playlist = {
   "Aerosmith": "Love in an elevator"
 }
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign({}, playlist, {[artistName]: songTitle})
+  return playlist
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
